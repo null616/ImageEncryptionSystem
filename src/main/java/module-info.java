7c862e40dcb2bacdde5a2aef13cee6c04dbc099c
lqf <laiyl1719@mails.jlu.edu.cn>
2023-02-06 22:37:37 +0800

    项目初始化

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module edu.jlu.csw.imageencryptionsystem {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                    requires org.kordamp.ikonli.javafx;
-            requires org.kordamp.bootstrapfx.core;
-            
+
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
     opens edu.jlu.csw.imageencryptionsystem to javafx.fxml;
     exports edu.jlu.csw.imageencryptionsystem;
 }
